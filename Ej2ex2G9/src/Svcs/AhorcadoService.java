971628a492593958196f9e8a320cb1d3b440c8a6
Nr <Nr@tbc.com>
2023-05-21 18:26:17 -0300

    Integr

--- Ej2ex2G9/src/Svcs/AhorcadoService.java
+++ Ej2ex2G9/src/Svcs/AhorcadoService.java
@@ -14,10 +14,10 @@ import java.util.Scanner;
  * @author U
  */
 public class AhorcadoService {
-    
-    
+
     Scanner scanner = new Scanner(System.in);
-    public Ahorcado crearJuego(){
+
+    public Ahorcado crearJuego() {
         Ahorcado Ahorc1 = new Ahorcado();
         String pal;
         System.out.print("Ing Palabra: ");
@@ -27,70 +27,77 @@ public class AhorcadoService {
         Ahorc1.setJugMax(scanner.nextInt());
         int l = pal.length();
         Ahorc1.setL(l);
-        for (int j = 0; j<l; j++){
+        for (int j = 0; j < l; j++) {
             char ch = pal.charAt(j);
             palVec[j] = ch;
         }
         Ahorc1.setVecPalab(palVec);
         return Ahorc1;
     }
-    public void longitud(Ahorcado Ahorc1){
+
+    public void longitud(Ahorcado Ahorc1) {
         System.out.println("Largo Palabra = " + Ahorc1.getVecPalab().length);
     }
-    
-    public void buscar(Ahorcado Ahorc1, char x){
+
+    public void buscar(Ahorcado Ahorc1, char x) {
         boolean b = false;
-        for (int j = 0; j<Ahorc1.getVecPalab().length ; j++){
-            if (Ahorc1.getVecPalab()[j]==x){
+        for (int j = 0; j < Ahorc1.getVecPalab().length; j++) {
+            if (Ahorc1.getVecPalab()[j] == x) {
                 b = true;
                 System.out.println("Se Encontro la letra ingresada!!");
                 break;
             }
         }
-        if (!b){
-        System.out.println("No se encontro la letra ingresada");
+        if (!b) {
+            System.out.println("No se encontro la letra ingresada");
         }
 
     }
-    public boolean encontradas(Ahorcado Ahorc1, char x){
+
+    public boolean encontradas(Ahorcado Ahorc1, char x) {
         boolean b = false;
-        int i=0;
+        int i = 0;
         int largoPal = Ahorc1.getVecPalab().length;
+        //System.out.println("Largo Pal= " + largoPal);
         int Econtradas = Ahorc1.getCantLetEnc();
+        //System.out.println("Letras Encontradas= " +Econtradas);
         int faltan = largoPal - Ahorc1.getCantLetEnc();
-        for (int j = 0; j<largoPal ; j++){
-            if (Ahorc1.getVecPalab()[j]==x){
+        for (int j = 0; j < largoPal; j++) {
+            if (Ahorc1.getVecPalab()[j] == x) {
                 i++;
             }
         }
-        b = i>0;
-        if (b){
-        System.out.println("Se incontraron"+ i + "letras");
-        faltan -=i;
-        Ahorc1.setCantLetEnc(Ahorc1.getCantLetEnc()+i);
-       
+        b = i > 0;
+        if (b) {
+            System.out.println("Se incontraron " + i + " letras");
+            faltan -= i;
+            Ahorc1.setCantLetEnc(Ahorc1.getCantLetEnc() + i);
+
+        } else {
+
         }
-        else{
-            
-        }
-    return b;
+        return b;
 
     }
-    public int intentos(Ahorcado Ahorc1){
-        int x = Ahorc1.getJugMax()-1;
+
+    public int intentos(Ahorcado Ahorc1) {
+        int x =  Ahorc1.getJugMax() -1;
+        Ahorc1.setJugMax(x);
         return x;
-        
+
     }
-    
-    public void juego(Ahorcado Ahorc1){
-        System.out.print("Ing una letra : ");
-        char letra = scanner.next().charAt(0);
-        crearJuego();
-        longitud(Ahorc1);
-        buscar(Ahorc1, letra);
-        encontradas(Ahorc1, letra);
-        intentos(Ahorc1);
+
+    public void juego(Ahorcado Ahorc1) {
+        do {
+            System.out.print("Ing una letra : ");
+            char letra = scanner.next().charAt(0);
+            //crearJuego();
+            longitud(Ahorc1);
+            buscar(Ahorc1, letra);
+            encontradas(Ahorc1, letra);
+            //System.out.println("Intentos= " + intentos(Ahorc1));
+        } while (intentos(Ahorc1) > 0);
+        System.out.println("Game Over!!");
     }
-    
-    
+
 }
